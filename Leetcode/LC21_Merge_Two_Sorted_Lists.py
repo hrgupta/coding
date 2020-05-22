3ee8922273fac089109e345b225f22510b9fbf73
hrg <hrgupta@outlook.com>
2020-05-22 13:57:01 -0500

    daily coding commit - lc

---
+++ Leetcode/LC21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,32 @@
+'''Leetcode Top Interview Questions
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+'''
+'''Solution
+208 / 208 test cases passed.
+Status: Accepted
+Runtime: 52 ms
+Memory Usage: 13.8 MB
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
